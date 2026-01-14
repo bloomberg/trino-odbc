b8623e01a756c029b412dfc241f9732ea4a4aa43
Akintoye Olorode <akintoye.olorode@gmail.com>
2026-01-14 11:31:28 -0500

    Fix crash in Device Flow Polling
    Fix crash in Device Flow Polling

--- src/trinoAPIWrapper/authProvider/deviceFlowAuthProvider.cpp
+++ src/trinoAPIWrapper/authProvider/deviceFlowAuthProvider.cpp
@@ -133,6 +133,10 @@ std::string refreshDeviceCredAuth(ClientCredAuthParams& params) {
       params.responseData->clear();
       params.responseHeaderData->clear();
 
+      headers = nullptr;
+      headers                    = curl_slist_append(
+          headers, "Content-Type: application/x-www-form-urlencoded");
+      curl_easy_setopt(params.curl, CURLOPT_HTTPHEADER, headers);
       // Send the polling request
       curl_easy_setopt(params.curl, CURLOPT_URL, tokenEndpoint.c_str());
       curl_easy_setopt(params.curl, CURLOPT_POSTFIELDS, tokenPost.c_str());
@@ -140,6 +144,8 @@ std::string refreshDeviceCredAuth(ClientCredAuthParams& params) {
       WriteLog(LL_DEBUG,
                "  Polling token endpoint HTTP response code: " +
                    std::to_string(res));
+      curl_slist_free_all(
+          headers); // Always free curl headers to avoid memory leaks
 
       // Parse the response
       json pollingResponse = json::parse(*params.responseData);
